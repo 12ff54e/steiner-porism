f67f8d36aeddfe90e5c0166277f1ade62c85100e
qzhong <12ff54e@gmail.com>
2021-10-28 19:19:25 +0800

    Add levi-civita symbol and kronecker delta.

--- test/test.cpp
+++ test/test.cpp
@@ -21,5 +21,17 @@ int main() {
     for (auto&& x : composite(t2, t1, t2)(pt)) { cout << x << ' '; };
     cout << '\n';
 
+    // Random generated tests for Levi-Civita symbol and Kronecker delta
+
+    static_assert(leviCivita(2, 4, 5, 7, 3, 1, 0, 6) == -1);
+    static_assert(leviCivita(7, 4, 2, 3, 5, 0, 6, 1) == 1);
+    static_assert(leviCivita(3, 6, 5, 1, 2, 7, 4, 0) == -1);
+    static_assert(leviCivita(0, 5, 4, 1, 6, 7, 3, 2) == 1);
+    static_assert(leviCivita(1, 0, 7, 5, 4, 7, 6, 2) == 0);
+
+    static_assert(kroneckerDelta(1, 1, 1) == true);
+    static_assert(kroneckerDelta(1, 0, 1) == false);
+    static_assert(kroneckerDelta(9, 9, 9, 9) == true);
+
     return 0;
 }
--- util.hpp
+++ util.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <cstddef>
 #include <type_traits>
 #include <utility>
@@ -17,3 +18,43 @@ using const_integer_sequence = typename const_integer_seq_impl<T, N, C>::type;
 
 template <std::size_t N, std::size_t C = std::size_t{0}>
 using const_index_sequence = const_integer_sequence<std::size_t, N, C>;
+
+/**
+ * @brief levi-civita symbol
+ *
+ * @tparam Args
+ * @param args any combination of int 0,...,N-1 (N is the size of Args)
+ * @return constexpr int
+ */
+template <typename... Args>
+constexpr int leviCivita(Args... args) {
+    std::array<int, sizeof...(Args)> arr{args...};
+    int lc = 1;
+
+    for (int i = 0; i < arr.size(); ++i) {
+        while (arr[i] != i) {
+            if (arr[i] == arr[arr[i]]) { return 0; }
+            int tmp = arr[i];
+            arr[i] = arr[tmp];
+            arr[tmp] = tmp;
+            lc = -lc;
+        }
+    }
+    return lc;
+}
+
+/**
+ * @brief Kronecker delta symbol, evaluate to true if all parameters are the
+ * same.
+ *
+ * @tparam T Type of 1st parameter
+ * @tparam Args Types of rest parameters, should be convertable to T, or at
+ * least can be comparable to T
+ * @param v 1st paremeter
+ * @param args other parameters
+ * @return constexpr bool
+ */
+template <typename T, typename... Args>
+constexpr bool kroneckerDelta(T v, Args... args) {
+    return (... && (v == args));
+}
